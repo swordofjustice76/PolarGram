0f3762b10becec5cc16cf7eea94259de9b2d9ae8
swordofjustice <damoproductionsandroid@gmail.com>
2019-10-03 12:01:31 +1000

    Add project files.

---
+++ PolarGram/PolarGram/SplashScreen.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PolarGram
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SplashScreen : ContentPage
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+
+
+            if (Device.RuntimePlatform == Device.Android)
+            {
+                Padding = new Thickness(0, 0, 0, 25);
+            }
+            else if (Device.RuntimePlatform == Device.iOS)
+            {
+                Padding = new Thickness(0, 25, 0, 0);
+            };
+
+
+        }
+    }
+}
